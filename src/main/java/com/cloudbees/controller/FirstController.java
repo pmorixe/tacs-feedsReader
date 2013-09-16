1f2d376c04fcc911df235339db5dae85213973b1
pmorixe <morixe.pablo@gmail.com>
2013-09-15 23:57:21 -0300

    Spring MVC dependencies

---
+++ src/main/java/com/cloudbees/controller/FirstController.java
@@ -0,0 +1,18 @@
+package com.cloudbees.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/ehGuacho")
+public class FirstController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+		model.addAttribute("message", "Spring MVC");
+        return "hello";
+    }
+}
